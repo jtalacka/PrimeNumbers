541190ecda06bc7f1bd296027a8a2e69ab4963ae
jtalacka <43013465+jtalacka@users.noreply.github.com>
2019-10-11 12:51:31 +0300

    v1

---
+++ PrimeNumber/Prime.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace PrimeNumber
+{
+    public class Prime
+    {
+        private int range1, range2;
+        public Prime() { }
+
+
+        public void setRange(int r1, int r2)
+        {
+            this.range1 = r1;
+            this.range2 = r2;
+        }
+        public void iterateRange()
+        {
+            int r1 = range1;
+            int r2 = range2;
+            while (r1 <= r2)// iterating through the range of range1 and range2
+            {
+                checkPrime(r1);
+                r1++;
+
+            }
+        }
+        private void checkPrime(int r1)
+        {
+            if (r1 <= 0)// checking if number is positive and not 0
+            {
+                return;
+            }
+            bool prime = true;
+            for (int i = 2; i < r1; i++)
+            {
+                if (r1 % i == 0)
+                {
+                    prime = false;
+                    break;
+                }
+            }
+            if (prime == true)
+            {
+                printPrime(r1);
+            }
+
+
+        }
+        private void printPrime(int prime)
+        {
+            Console.Write(prime + " ");
+
+        }
+
+    }
+}
---
+++ PrimeNumber/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Prime prime = new Prime();
+            input(prime);
+            prime.iterateRange();
+            Console.ReadKey();
+
+        }
+        public static void input(Prime prime)
+        {
+            string range;
+            Console.WriteLine("Please write two numbers");
+            range = Console.ReadLine();
+            string[] values = range.Split(' ');
+            try {
+
+                if (values.Length == 2)// checking if there'es more than two numbers
+                {
+                    int r1, r2;
+                    r1 = Int32.Parse(values[0]);
+                    r2 = Int32.Parse(values[1]);
+                    if (r1<r2)
+                    {
+                        prime.setRange(r1,r2);
+                    }
+                    else
+                    {
+                        prime.setRange(r2,r1);
+                    }
+
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect input, please try again");
+                    input(prime);
+                }
+
+
+            }
+            catch(Exception ex)// this opens up when there's symbols involved
+            {
+            
+                Console.WriteLine("Incorrect input, please try again");
+                input(prime);
+            }
+        }
+    }
+}
